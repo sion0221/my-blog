2678ea0676e791fe76c315a241af4842f8b1e4ae
Heosion <tldhs123e@gmail.com>
2026-01-29 14:17:24 +0900

    로고 파비콘 이미지 추가

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,7 +1,9 @@
+import logo from "public/logo/SION_LOG.svg";
+
 export const Logo = () => {
   return (
     <img
-      src="/public/logo/SION_LOG.svg"
+      src={logo.src}
       alt="SION.LOG Logo"
       className="w-30 h-auto hover:opacity-80 transition-opacity object-contain"
     />
